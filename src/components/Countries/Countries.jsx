5b0d21d888c57c0cf6e00bdd7e3466aed189e649
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-10-20 12:42:12 +0600

    Dynamic Works in buttons

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -2,10 +2,12 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Country2 from "../Country2/Country2";
 // import Country from "../Country/Country";
-import './Countries.css';
+import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -13,16 +15,34 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
 
+  const handleVisitedCountry = country => {
+    console.log('add this to your visited country');
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  }
+
   return (
     <div>
-      <h3>Countries: {countries.length}</h3>
+      <h3>Countries: {countries.length}
+      </h3>
+
+      <div>
+        <h5>Visited Countries: {visitedCountries.length}</h5>
+        <ul>
+            {
+                visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+            }
+        </ul>
+      </div>
       <div className="country-container">
         {/* {
                 countries.map(country => <Country key={country.cca3} country={country}></Country>)
             } */}
-      {countries.map((country2) => (
-        <Country2 key={country2.cca3} country2={country2}></Country2>
-      ))}
+        {countries.map((country2) => (
+          <Country2 key={country2.cca3}
+          handleVisitedCountry = {handleVisitedCountry} 
+          country2={country2}></Country2>
+        ))}
       </div>
     </div>
   );
--- src/components/Country2/Country2.jsx
+++ src/components/Country2/Country2.jsx
@@ -1,25 +1,40 @@
 import { useState } from "react";
 import "./Country2.css";
-const Country2 = ({ country2 }) => {
+const Country2 = ({ country2, handleVisitedCountry }) => {
   const { name, flags, continents, capital, population, cca3 } = country2;
 
   const [visited, setVisited] = useState(false);
 
   const handleVisited = () => {
     setVisited(!visited);
-  }
+  };
+
+//   console.log(handleVisitedCountry);
+
+//   const passWithParams = () => {
+//     handleVisitedCountry(country2);
+//   }
 
   return (
-    <div className="country">
-      <h3>Country Name: {name.common}</h3>
+    <div className={`country ${visited ? "visited" : "Non-visited"}`}>
+      <h3 style={{ color: visited ? "purple" : "white" }}>
+        Country Name: {name.common}
+      </h3>
+
       <img className="img" src={flags.png} alt="" />
+
       <p>Continent: {continents}</p>
       <p>Capital: {capital}</p>
       <p>Population: {population}</p>
-      <p><small>Code: {cca3}</small></p>
+      <p>
+        <small>Code: {cca3}</small>
+      </p>
+      <button onClick={() => handleVisitedCountry(country2)}>
+       Mark Visited</button>
+      <br />
 
-      <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
-      {visited ?'I have visited these country.' : 'I want to visit'}
+      <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+      {visited ? "I have visited these country." : "I want to visit"}
     </div>
   );
 };
